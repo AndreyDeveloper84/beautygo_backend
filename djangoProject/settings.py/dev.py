f3e3e3553cd308e40283383dbf3427bafeb9a358
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-04-27 17:49:09 +0300

    Refactor settings: split into base, dev, prod

--- djangoProject/settings.py/dev.py
+++ djangoProject/settings.py/dev.py
@@ -10,3 +10,10 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
--- djangoProject/settings.py/prod.py
+++ djangoProject/settings.py/prod.py
@@ -3,3 +3,10 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ["localhost"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
