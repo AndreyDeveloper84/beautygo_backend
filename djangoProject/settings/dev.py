5364135e0b8e6038ffc64fc46567e8a63de6cdbf
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-04-27 22:25:29 +0300

    CORS_ALLOWED_ORIGINS = ['*']

--- djangoProject/settings/dev.py
+++ djangoProject/settings/dev.py
@@ -5,9 +5,7 @@ DEBUG = True
 INSTALLED_APPS += ['corsheaders']
 MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware'] + MIDDLEWARE
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-]
+CORS_ALLOWED_ORIGINS = ['*']
 
 ALLOWED_HOSTS = ["*"]
 
